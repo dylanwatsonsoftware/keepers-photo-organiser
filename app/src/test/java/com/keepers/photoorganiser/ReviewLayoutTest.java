3815173f57548ceec52bde8ebe813a34af907e9f
Dylan Watson <lotsabackscatter@gmail.com>
2026-09-07 22:33:25 +0800

    Respect status bar in photo review

---
+++ app/src/test/java/com/keepers/photoorganiser/ReviewLayoutTest.java
@@ -0,0 +1,20 @@
+package com.keepers.photoorganiser;
+
+import static org.junit.Assert.assertTrue;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+
+@RunWith(RobolectricTestRunner.class)
+public class ReviewLayoutTest {
+    @Test public void reviewContentRespectsSystemWindowInsets() {
+        View layout = LayoutInflater.from(RuntimeEnvironment.getApplication())
+                .inflate(R.layout.activity_review, null);
+
+        assertTrue(layout.getFitsSystemWindows());
+    }
+}
